0330c77c36cd2f330d8a86098eac0890d4dd6e93
Miri Babayev <veagnox@gmail.com>
2019-11-26 20:45:14 +0400

    Authentication incomplete

--- src/main/java/authentication/mainCredentials.java
+++ src/main/java/authentication/mainCredentials.java
@@ -1,4 +1,19 @@
 package authentication;
 
-public class mainCredentials {
+import java.util.HashMap;
+import java.util.Map;
+
+public class mainCredentials implements mainAuth{
+
+    Map<String, String> credentials = new HashMap<>(); //TODO change to database input model
+
+    public mainCredentials(String uid, String pwd) {
+        credentials.put(uid, pwd); //TODO Implement database update model
+    }
+
+    @Override
+    public boolean check(String uid, String pwd) {
+        return credentials.containsKey(uid) && credentials.get(uid).equals(pwd); //TODO Implement database check model
+    }
+    
 }
