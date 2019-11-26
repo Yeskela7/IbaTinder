40750105bf5da3a411ce33116565315079e90f20
Aleksey Hajialiyev <Yeskela7@users.noreply.github.com>
2019-11-26 13:00:06 +0400

    --- Iba Tinder init

---
+++ src/main/java/dao/interfaces/Dao.java
@@ -0,0 +1,16 @@
+package dao.interfaces;
+
+import java.util.List;
+
+public interface Dao<T> {
+    T get(int id);
+
+    List<T> getAll();
+
+    void save(T t);
+
+    void update(T t);
+
+    void delete(T t);
+
+}
