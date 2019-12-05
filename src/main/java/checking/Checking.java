b0b289aa03eea862e2b81094240ad222e7e703d5
Aleksey Hajialiyev <Yeskela7@users.noreply.github.com>
2019-12-05 23:44:20 +0400

    --- checking (start)

--- src/main/java/checking/Checking.java
+++ src/main/java/checking/Checking.java
@@ -8,10 +8,21 @@ import java.util.stream.Collectors;
 
 public class Checking {
 
-    public static boolean checkRegister(ServletRequest req, UsersDaoService uds){
+    public static boolean isUniqueUser(ServletRequest req, UsersDaoService uds){
         String email = req.getParameter("email");
         return uds.getAllUsers().stream().noneMatch(user -> user.getEmail().equalsIgnoreCase(email));
     }
 
+    public static boolean checkInputRegister(ServletRequest req){
+
+        String login = req.getParameter("email");
+        String password = req.getParameter("password");
+        String name = req.getParameter("email");
+        String surname = req.getParameter("password");
+        String picUrl = req.getParameter("email");
+        String age = req.getParameter("password");
+
+
+    }
 
 }
