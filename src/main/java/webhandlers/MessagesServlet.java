5f22b826f2ee418ba5faa043f66131096c6e7e39
fidansadirli <56449996+fidansadirli@users.noreply.github.com>
2019-12-08 17:23:49 +0400

    MessageServlet

--- src/main/java/webhandlers/MessagesServlet.java
+++ src/main/java/webhandlers/MessagesServlet.java
@@ -1,6 +1,48 @@
 package webhandlers;
 
+import dao.services.MessagesDaoService;
+import template_engine.TemplateEngine;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class MessagesServlet extends HttpServlet {
+    private int senderID;
+    private int receiverID;
+    private MessagesDaoService service;
+
+    public MessagesServlet(){
+        senderID = -1;
+        receiverID = -1;
+        service = new MessagesDaoService();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie oneCookie : cookies){
+            if (oneCookie.getName().equals("%ID%")){
+                senderID = Integer.parseInt(oneCookie.getValue());
+            }
+        }
+        receiverID = Integer.parseInt(request.getParameter("id"));
+        TemplateEngine engine = new TemplateEngine("./content");
+        HashMap<String, Object> data = new HashMap<>();
+        try {
+            data.put("messages", service.getMessagesPair(senderID, receiverID));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        engine.render("chat.ftl", data, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        String text = request.getParameter("message");
+        service.saveMessage(senderID, receiverID, text);
+    }
 }
